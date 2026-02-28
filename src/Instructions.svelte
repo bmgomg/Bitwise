9d9e9a8cb04b7630c77eb7a82adf601426fefb5e
Arkady Elterman <elterman@gmail.com>
2026-02-28 23:41:04 +0000

    .

--- src/Instructions.svelte
+++ src/Instructions.svelte
@@ -6,7 +6,7 @@
 <div class="instr {_prompt.id || ss.over ? 'hidden' : ''}">
     {#if !_prompt.id}
         Combine the two inputs from the queue by applying an available operator above. The selected operator will become unavailable on the
-        next turn. To win, score at least {POINTS_TO_WIN} points with a 2-point lead.
+        next turn. To win, score at least {POINTS_TO_WIN} points with a 2-point lead after both players have completed the same number of turns.
     {/if}
 </div>
 
@@ -16,10 +16,11 @@
         place-self: center;
         display: grid;
         font-family: UI;
-        font-size: 15.6px;
+        font-size: 15.4px;
         filter: drop-shadow(0 0 3px #00000080);
         color: var(--blue);
         transition: opacity 0.2s;
+        translate: 0 -10px;
     }
 
     .hidden {
